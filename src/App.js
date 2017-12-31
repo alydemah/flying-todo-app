e677ece982b1c2548770ffafb59fdf4dc405be78
Mohamed Aly <alydemah@gmail.com>
2017-12-31 17:50:41 +0100

    adding basic components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import AddItem from './components/AddItem';
-import TodoItems from './components/TodoItems';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="page-header">
-          <h1>Flying ToDo App</h1>
-          </div>
-        <hr/>
-        <AddItem />
-        <div>Todo List</div>
-        <TodoItems />
+        <Header />
+
+
+        <MainContent />
 
       </div>
     );
---
+++ src/MainContent/TodoList/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+const TodoList = props =>
+    <ul>
+
+
+        <Todo
+            key={index}
+            name={todo.title}
+            
+        />
+
+     </ul>;
+     
+     
+
+     
+     export default TodoList;
